1d228aea021efcfd130a4bbf69cfa45319ad5a44
Tempus23 <carlos.cbm32@gmail.com>
2025-02-21 11:35:57 +0100

    mas modelos y datasets

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 RANDOM_SEED = 78
-DATASET_PATH = 'dataset/original_data'
-PROCESSED_DATA_PATH = 'dataset/processed_data'
-SPLIT_PATH = 'dataset/split'
 
 MENDELEY_OAI_224_SPLIT_PATH = 'dataset/mendeleyOAI_dataset/ClsKLData/kneeKL224'
 MENDELEY_OAI_299_SPLIT_PATH = 'dataset/mendeleyOAI_dataset/ClsKLData/kneeKL299'
+MENDELEY_OAI_BRIGHT_200_PATH = 'dataset/mendeleyOAI_dataset/brightness_200'
+MENDELEY_OAI_BRIGHT_200_SPLIT_PATH = 'dataset/mendeleyOAI_dataset/brightness_200_split'
+
 
 MENDELEY_EXPERT1_PATH = 'dataset\mendeley_dataset\MedicalExpert-I'
 MENDELEY_EXPERT1_SPLIT_PATH = 'dataset/mendeley_dataset/medical_expert_1_split'
--- src/models/efficientnet.py
+++ src/models/efficientnet.py
@@ -1,20 +1,18 @@
 from torch import nn, Tensor
 import torch
 from typing import Optional
-import timm
 from torchvision import models
-from torchvision.models import EfficientNet_B5_Weights
+from torchvision.models import *
 
+# Clase EfficientNetB5Custom
 class EfficientNetB5Custom(nn.Module):
-    def __init__(self, num_classes=5, pretrained = True):
+    def __init__(self, num_classes=5, pretrained=True):
         super(EfficientNetB5Custom, self).__init__()
-        # Cargar el modelo EfficientNetB5 preentrenado
         if pretrained:
             self.efficientnet = models.efficientnet_b5(weights=EfficientNet_B5_Weights.DEFAULT)
         else:
             self.efficientnet = models.efficientnet_b5(weights=None)
         self.name = "EfficientNetB5Custom"
-        # Reemplazar la capa final del clasificador
         in_features = self.efficientnet.classifier[1].in_features
         self.efficientnet.classifier = nn.Sequential(
             nn.Linear(in_features, 256),
@@ -24,6 +22,66 @@ class EfficientNetB5Custom(nn.Module):
             nn.Linear(256, num_classes),
             nn.Softmax(dim=1)
         )
-        
+    
+    def forward(self, x):
+        return self.efficientnet(x)
+
+# Clase EfficientNetB5
+class EfficientNetB5(nn.Module):
+    def __init__(self, num_classes=5, pretrained=True):
+        super(EfficientNetB5, self).__init__()
+        if pretrained:
+            self.efficientnet = models.efficientnet_b5(weights=EfficientNet_B5_Weights.DEFAULT)
+        else:
+            self.efficientnet = models.efficientnet_b5(weights=None)
+        self.name = "EfficientNetB5"
+        in_features = self.efficientnet.classifier[1].in_features
+        self.efficientnet.classifier[1] = nn.Linear(in_features, num_classes)
+    
+    def forward(self, x):
+        return self.efficientnet(x)
+
+# Clase EfficientNetB0
+class EfficientNetB0(nn.Module):
+    def __init__(self, num_classes=5, pretrained=True):
+        super(EfficientNetB0, self).__init__()
+        if pretrained:
+            self.efficientnet = models.efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT)
+        else:
+            self.efficientnet = models.efficientnet_b0(weights=None)
+        self.name = "EfficientNetB0"
+        in_features = self.efficientnet.classifier[1].in_features
+        self.efficientnet.classifier[1] = nn.Linear(in_features, num_classes)
+    
+    def forward(self, x):
+        return self.efficientnet(x)
+
+# Clase EfficientNetB4
+class EfficientNetB4(nn.Module):
+    def __init__(self, num_classes=5, pretrained=True):
+        super(EfficientNetB4, self).__init__()
+        if pretrained:
+            self.efficientnet = models.efficientnet_b4(weights=EfficientNet_B4_Weights.DEFAULT)
+        else:
+            self.efficientnet = models.efficientnet_b4(weights=None)
+        self.name = "EfficientNetB4"
+        in_features = self.efficientnet.classifier[1].in_features
+        self.efficientnet.classifier[1] = nn.Linear(in_features, num_classes)
+    
+    def forward(self, x):
+        return self.efficientnet(x)
+
+# Clase EfficientNetB7
+class EfficientNetB7(nn.Module):
+    def __init__(self, num_classes=5, pretrained=True):
+        super(EfficientNetB7, self).__init__()
+        if pretrained:
+            self.efficientnet = models.efficientnet_b7(weights=EfficientNet_B7_Weights.DEFAULT)
+        else:
+            self.efficientnet = models.efficientnet_b7(weights=None)
+        self.name = "EfficientNetB7"
+        in_features = self.efficientnet.classifier[1].in_features
+        self.efficientnet.classifier[1] = nn.Linear(in_features, num_classes)
+    
     def forward(self, x):
         return self.efficientnet(x)
